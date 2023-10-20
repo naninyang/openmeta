d785ce8edb4c955294725199a785e626e5c23066
Chloe <gtalk.with.nani@gmail.com>
2023-10-20 18:03:28 +0900

    .

---
+++ pages/api/og.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import ogs from "open-graph-scraper";
+
+async function fetchOpenGraphData(url: string) {
+  const data = await ogs({ url });
+  if (data.error) {
+    throw new Error(data.result.error);
+  }
+  return data.result;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { url } = req.query;
+
+  try {
+    const rawData = await fetchOpenGraphData(url as string);
+
+    const filteredData = {
+      ogTitle: rawData.ogTitle,
+      ogUrl: rawData.ogUrl,
+      ogImage: rawData.ogImage ? rawData.ogImage[0].url : null,
+      ogDescription: rawData.ogDescription,
+      ogCreator: rawData.ogArticleAuthor || rawData.twitterCreator,
+    };
+
+    return res.status(200).json(filteredData);
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to fetch data" });
+  }
+}

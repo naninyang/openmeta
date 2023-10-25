67e64e3a20741a1e2588302a8453a6271c56e0d5
Chloe <gtalk.with.nani@gmail.com>
2023-10-25 20:06:11 +0900

    Support EUC-KR

--- pages/api/og.ts
+++ pages/api/og.ts
@@ -1,12 +1,40 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import ogs from "open-graph-scraper";
+import axios from "axios";
+import iconv from "iconv-lite";
+import cheerio from "cheerio";
 
 async function fetchOpenGraphData(url: string) {
-  const data = await ogs({ url });
-  if (data.error) {
-    throw new Error(data.result.error);
-  }
-  return data.result;
+  const response = await axios.get(url, { responseType: "arraybuffer" });
+  const html = iconv.decode(response.data, "UTF-8");
+  const $$ = cheerio.load(html);
+  const contentType = $$('meta[http-equiv="Content-Type"]').attr("content");
+  const charset = contentType?.includes("charset=")
+    ? contentType.split("charset=")[1]
+    : "UTF-8";
+
+  let data = iconv.decode(response.data, charset);
+
+  let $ = cheerio.load(data);
+  const ogUrl = $('meta[property="og:url"]').attr("content");
+  const ogTitle =
+    $('meta[property="og:title"]').attr("content") || $("title").html();
+  const ogDescription =
+    $('meta[property="og:description"]').attr("content") ||
+    $('meta[name="description"]').attr("content");
+  const ogImage = $('meta[property="og:image"]').attr("content");
+  const ogArticleAuthor =
+    $('meta[property="og:article:author"]').attr("content") ||
+    $('meta[name="author"]').attr("content");
+  const ogSiteName = $('meta[property="og:site_name"]').attr("content");
+
+  return {
+    ogUrl,
+    ogTitle,
+    ogDescription,
+    ogImage,
+    ogArticleAuthor,
+    ogSiteName,
+  };
 }
 
 export default async function handler(
@@ -22,7 +50,7 @@ export default async function handler(
       ogTitle: rawData.ogTitle,
       ogDescription: rawData.ogDescription,
       ogUrl: rawData.ogUrl,
-      ogImage: rawData.ogImage ? rawData.ogImage[0].url : null,
+      ogImage: rawData.ogImage,
       ogCreator: rawData.ogArticleAuthor,
       ogSiteName: rawData.ogSiteName,
     };
